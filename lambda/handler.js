dd966eeba05d2891e48f8159b10a04d4a6721aaf
Michael J. Cohen <mjc@kernel.org>
2017-04-10 18:11:55 -0600

    stuff loads at least

--- lambda/handler.js
+++ lambda/handler.js
@@ -1,20 +1,10 @@
 'use strict';
 
-console.log('Loading function');
+const request = require('request');
 
-get_chargebee_plan = (chargebee,plan_id) => {
-  var plan;
-  chargebee.plan.retrieve(plan_id).request(
-  function(error,result){
-    if(error){
-      console.log(error);
-    }else{
-      console.log(result);
-      plan = result.plan;
-    }
-  });
-  plan;
-};
+var get_chargebee_plan = (plan_id) => {
+  return plan_id;
+}
 
 exports.handler = (event, context, callback) => {
   var body = JSON.parse(event.body);
@@ -27,13 +17,7 @@ exports.handler = (event, context, callback) => {
   var quantity = subscription.plan_quantity;
   var plan_id = subscription.plan_id;
 
-  var chargebee = require("chargebee");
-  chargebee.configure({
-    site: "imazen-test",
-    api_key: "test_bOJ3nmQFjkduvQcG1coxEYxuqdZaYJpx"
-  });
-
-  var plan = get_chargebee_plan(chargebee,plan_id);
+  var plan = get_chargebee_plan(plan_id);
 
   console.log('Event Type', event_type);
   console.log('Subscription: ', JSON.stringify(subscription, null, 2));
