73306e09ec590549c5f655f09237c9a0d3c3399e
Michael J. Cohen <mjc@kernel.org>
2017-04-19 16:55:08 -0600

    Add failing tests

--- imazen_licensing/test/test_license_generator.rb
+++ imazen_licensing/test/test_license_generator.rb
@@ -37,6 +37,13 @@ module ImazenLicensing
       }
     end
 
+    def formatted(license)
+      "Kind: #{license[:kind]}
+      Sku: #{license[:sku]}
+      Domain: #{license[:domain]}
+      Features: #{licenses[:features].join(' ')}"
+    end
+
     def key
       @key ||= File.read("#{File.dirname(__FILE__)}/support/test_private_key.pem")
     end
@@ -46,7 +53,19 @@ module ImazenLicensing
     end
 
     def test_subscription_license
-      LicenseGenerator.generate(subscription_license, key, passphrase)
+      generated = LicenseGenerator.generate(subscription_license, key, passphrase)
+      summary, body, signed = generated.split(':').map(&:strip)
+      assert_equal summary, ""
+      assert_equal Base64.strict_decode64(body), formatted(subscription_license)
+      assert verify_rsa(signed, body, key, passphrase)
+    end
+
+    def verify_rsa(signed, body, key, passphrase)
+      signature_bytes = Base64.strict_decode64(signed)
+      rsa = OpenSSL::PKey::RSA.new(key, passphrase)
+      digest = rsa.public_decrypt(signature_bytes)
+      body_digest = OpenSSL::Digest::SHA512.new.digest(body)
+      digest == body_digest
     end
   end
 end
