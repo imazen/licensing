78fe34cd6af1c33cd6495f4f85b41f1ffb60c5c4
Viki Harrod <victoria.harrod@gmail.com>
2019-07-22 18:08:31 -0600

    Add missing gems; set up VCR

--- web/test/controllers/chargebee_controller_test.rb
+++ web/test/controllers/chargebee_controller_test.rb
@@ -1,9 +1,17 @@
 require 'test_helper'
 
 class ChargebeeControllerTest < ActionDispatch::IntegrationTest
-  test "should post index" do
-    post chargebee_index_url
-    assert_response :success
+  setup do
+    VCR.insert_cassette('chargebee')
+  end
+
+  teardown do
+    VCR.eject_cassette
+  end
+
+  test "forbidden without key" do
+    post chargebee_index_url, params: load_chargebee_params('subscription_created').reject { |k| k == :key }
+    assert_response :forbidden
   end
 
   test "subscription_created webhook" do
--- web/test/test_helper.rb
+++ web/test/test_helper.rb
@@ -2,9 +2,15 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
+# Store test http interactions so we don't have to make real web requests every test run
+VCR.configure do |config|
+  config.cassette_library_dir = "test/support/vcr_cassettes"
+  config.hook_into :webmock
+end
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
+  # fixtures :all
 
   # Add more helper methods to be used by all tests here...
 end
