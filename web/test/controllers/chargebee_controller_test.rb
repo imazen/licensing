62284c69fb05dd71648c9247ed53ba330a6be425
Viki Harrod <victoria.harrod@gmail.com>
2019-07-24 15:16:31 -0600

    Add missing webhook tests

--- web/test/controllers/chargebee_controller_test.rb
+++ web/test/controllers/chargebee_controller_test.rb
@@ -1,23 +1,53 @@
 require 'test_helper'
 
 class ChargebeeControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    VCR.insert_cassette('chargebee')
-  end
-
-  teardown do
-    VCR.eject_cassette
-  end
-
   test "forbidden without key" do
-    post chargebee_index_url, params: load_chargebee_params('subscription_created').reject { |k| k == :key }
-    assert_response :forbidden
+    VCR.use_cassette("missing_key") do
+      post chargebee_index_url, params: load_chargebee_params('subscription_created').reject { |k| k == :key }
+      assert_response :forbidden
+    end
   end
 
   test "subscription_created webhook" do
-    post chargebee_index_url, params: load_chargebee_params('subscription_created')
-    assert_response :no_content
-    #assert_subscription_valid
+    VCR.use_cassette("subscription_created") do
+      post chargebee_index_url, params: load_chargebee_params('subscription_created')
+      assert_response :no_content
+    end
+  end
+
+  test "subscription_activated webhook" do
+    VCR.use_cassette("subscription_activated") do
+      post chargebee_index_url, params: load_chargebee_params('subscription_activated')
+      assert_response :no_content
+    end
+  end
+
+  test "subscription_cancelled webhook" do
+    VCR.use_cassette("subscription_cancelled") do
+      post chargebee_index_url, params: load_chargebee_params('subscription_cancelled')
+      assert_response :no_content
+    end
+  end
+
+  test "subscription_changed webhook" do
+    VCR.use_cassette("subscription_changed") do
+      post chargebee_index_url, params: load_chargebee_params('subscription_changed')
+      assert_response :no_content
+    end
+  end
+
+  test "subscription_reactivated webhook" do
+    VCR.use_cassette("subscription_reactivated") do
+      post chargebee_index_url, params: load_chargebee_params('subscription_reactivated')
+      assert_response :no_content
+    end
+  end
+
+  test "subscription_renewed webhook" do
+    VCR.use_cassette("subscription_renewed") do
+      post chargebee_index_url, params: load_chargebee_params('subscription_renewed')
+      assert_response :no_content
+    end
   end
 
   def load_chargebee_params(event_type)
