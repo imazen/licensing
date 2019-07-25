98ed135c42cf53daa87263e56adf9cd1e66ba9c7
Viki Harrod <victoria.harrod@gmail.com>
2019-07-24 18:40:20 -0600

    Update email bcc list

--- web/app/mailers/application_mailer.rb
+++ web/app/mailers/application_mailer.rb
@@ -1,3 +1,10 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "sales@imazen.io"
+
+  OUR_EMAILS = [
+    'lilith@imazen.io',
+    'imazensales@gmail.com',
+    'f5d5r0v0l2y2r4j6@imazen.slack.com'
+  ]
+
 end
--- web/app/mailers/license_mailer.rb
+++ web/app/mailers/license_mailer.rb
@@ -1,14 +1,5 @@
 class LicenseMailer < ApplicationMailer
-  OUR_EMAILS = [
-    'nathanael.jones@gmail.com',
-    'michael.joseph.cohen+imazen+licensing@gmail.com',
-    'imazensales@gmail.com',
-    'f5d5r0v0l2y2r4j6@imazen.slack.com'
-  ]
-
-
   def id_license_email(emails:, id_license_encoded:, id_license_text:, remote_license_text:)
-
     @id_license_body = id_license_encoded
     @id_license_text = id_license_text
     @remote_license_text = remote_license_text
