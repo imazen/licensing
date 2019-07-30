8fd4d6f95f4275be23ba0d7921c49026e2655ebe
Viki Harrod <victoria.harrod@gmail.com>
2019-07-29 20:49:36 -0600

    Finish removing database/ActiveRecord stuff

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,7 @@ require_relative 'boot'
 
 require "rails"
 # Pick the frameworks you want:
-require "active_model/railtie"
+# require "active_model/railtie"
 require "active_job/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
