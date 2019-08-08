d203bd2d8ac63239edf610270286bd188e8116cb
Viki Harrod <victoria.harrod@gmail.com>
2019-08-08 16:33:39 -0600

    Add subscription_stale? helper method, test license generator

--- spec/models/chargebee_parse_spec.rb
+++ spec/models/chargebee_parse_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe 'ChargebeeParse' do
-  subject { ChargebeeParse.new(params) }
+  subject { ChargebeeParse.new({"content" => content}) }
+  let(:content) {
+    { "subscription" => subscription_data }
+  }
 
   describe '#licensed_domains' do
-    let(:params) { {
-      "content" => {
-        "subscription" => {
-          "cf_licensed_domains" => "example.com also.example.com"
-        }
-      }
-    } }
+    let(:subscription_data) {
+      { "cf_licensed_domains" => domains.join(" ") }
+    }
+    let(:domains) { ['example.com', 'also.example.com'] }
 
     it 'returns an array of domains' do
-      expect(subject.licensed_domains).to eq ['example.com', 'also.example.com']
+      expect(subject.licensed_domains).to eq domains
     end
   end
 end
